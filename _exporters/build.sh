2148a81bf62fabf5449d1c309ee864ef66f1dfdd
sam@23.199 <you@example.com>
2022-11-17 11:56:11 +0800

    sq: _exporters; build --- rename _exporters
    +yq
    
    +base, oth
    
    docker push $img
    
    build
    
    action: exporters-down
    
    notes github action
    
    up ig
    
    up pack

---
+++ _exporters/build.sh
@@ -0,0 +1,14 @@
+source /etc/profile
+export |grep DOCKER_REG
+repo=registry.cn-shenzhen.aliyuncs.com
+echo "${DOCKER_REGISTRY_PW_infrastSubUser2}" |docker login --username=${DOCKER_REGISTRY_USER_infrastSubUser2} --password-stdin $repo
+
+# repoHub=docker.io
+# echo "${DOCKER_REGISTRY_PW_dockerhub}" |docker login --username=${DOCKER_REGISTRY_USER_dockerhub} --password-stdin $repoHub
+        
+ns=infrastlabs
+ver=v5
+
+img=$repo/$ns/prom-exporters-down:latest
+docker build -t $img .
+docker push $img
--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 export CGO_ENABLED=0
 go build -o prometheus-exporter-merger -v -ldflags "-s -w $flags" ./
+
+
+tar -zcvf prometheus-exporter-merger.tar.gz prometheus-exporter-merger
+ls -lh prometheus-exporter-merger*
